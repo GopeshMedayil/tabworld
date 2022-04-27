f29d3874f91d85b35091a25c3b950adaf0dc6bcd
GopeshMedayil <medayilgopesh@gmail.com>
2022-04-28 00:07:13 +0530

    Added hover bookmark list css

--- src/components/Bookmark/BookmarkList.js
+++ src/components/Bookmark/BookmarkList.js
@@ -14,24 +14,36 @@ function BookmarkList() {
                 </div>
             </div>
             <div class="bookmark-item">
+                <div>
+                    <i class="bi bi-x-lg float-end px-2"></i>
+                </div>
                 <div className="item-warpper">
                     <div className="initial-letter">G</div>
                     <div className="item-link">Google</div>
                 </div>
             </div>
             <div class="bookmark-item">
+                <div>
+                    <i class="bi bi-x-lg float-end px-2"></i>
+                </div>
                 <div className="item-warpper">
                     <div className="initial-letter">G</div>
                     <div className="item-link">Google</div>
                 </div>
             </div>
             <div class="bookmark-item">
+                <div>
+                    <i class="bi bi-x-lg float-end px-2"></i>
+                </div>
                 <div className="item-warpper">
                     <div className="initial-letter">G</div>
                     <div className="item-link">Google</div>
                 </div>
             </div>
             <div class="bookmark-item">
+                <div>
+                    <i class="bi bi-x-lg float-end px-2"></i>
+                </div>
                 <div className="item-warpper">
                     <div className="initial-letter">G</div>
                     <div className="item-link">Google</div>
