e4c39dd5a121ecea43eb15b84744f070a9c16e88
GopeshMedayil <medayilgopesh@gmail.com>
2022-04-27 23:59:44 +0530

    css wip

--- src/components/Bookmark/BookmarkList.js
+++ src/components/Bookmark/BookmarkList.js
@@ -4,6 +4,10 @@ function BookmarkList() {
     return (
         <div className="bookmark-list">
             <div class="bookmark-item">
+                <div>
+                    <i class="bi bi-x-lg float-end px-2"></i>
+                </div>
+
                 <div className="item-warpper">
                     <div className="initial-letter">G</div>
                     <div className="item-link">Google</div>
