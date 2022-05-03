d9e72172c92ef694b414281f9befaa0f07a573eb
GopeshMedayil <medayilgopesh@gmail.com>
2022-05-03 15:54:33 +0530

    Added bookamrk data

--- src/components/Bookmark/BookmarkList.js
+++ src/components/Bookmark/BookmarkList.js
@@ -1,54 +1,28 @@
 import './Bookmark.css';
+import { useSelector } from 'react-redux';
 
 function BookmarkList() {
+
+    const deleteBookmark = () => {
+        console.log('deleteBookmark');
+    }
+    const bookmarks = useSelector(state => state.bookmark.bookmarks);
+    console.log("Bookmark list", bookmarks);
     return (
         <div className="bookmark-list">
-            <div class="bookmark-item">
-                <div>
-                    <i class="bi bi-x-lg float-end px-2"></i>
-                </div>
+            {bookmarks && bookmarks.map(bookmark => (
+                <div key={bookmark.id} className="bookmark-item">
+                    <div>
+                        <i className="bi bi-x-lg float-end px-2" onClick={deleteBookmark}></i>
+                    </div>
 
-                <div className="item-warpper">
-                    <div className="initial-letter">G</div>
-                    <div className="item-link">Google</div>
+                    <div className="item-warpper">
+                        <div className="initial-letter">{bookmark.name.charAt(0)}</div>
+                        <div className="item-link"><a href={bookmark.url} target="blank">{bookmark.name}</a></div>
+                    </div>
                 </div>
-            </div>
-            <div class="bookmark-item">
-                <div>
-                    <i class="bi bi-x-lg float-end px-2"></i>
-                </div>
-                <div className="item-warpper">
-                    <div className="initial-letter">G</div>
-                    <div className="item-link">Google</div>
-                </div>
-            </div>
-            <div class="bookmark-item">
-                <div>
-                    <i class="bi bi-x-lg float-end px-2"></i>
-                </div>
-                <div className="item-warpper">
-                    <div className="initial-letter">G</div>
-                    <div className="item-link">Google</div>
-                </div>
-            </div>
-            <div class="bookmark-item">
-                <div>
-                    <i class="bi bi-x-lg float-end px-2"></i>
-                </div>
-                <div className="item-warpper">
-                    <div className="initial-letter">G</div>
-                    <div className="item-link">Google</div>
-                </div>
-            </div>
-            <div class="bookmark-item">
-                <div>
-                    <i class="bi bi-x-lg float-end px-2"></i>
-                </div>
-                <div className="item-warpper">
-                    <div className="initial-letter">G</div>
-                    <div className="item-link">Google</div>
-                </div>
-            </div>
+            ))}
+
         </div>
     )
 
