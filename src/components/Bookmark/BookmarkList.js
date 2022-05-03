3877191a2be69d6933757bab7915fd347747a78f
GopeshMedayil <medayilgopesh@gmail.com>
2022-05-03 18:22:08 +0530

    bug fix multiple events callings on delete click

--- src/components/Bookmark/BookmarkList.js
+++ src/components/Bookmark/BookmarkList.js
@@ -2,45 +2,62 @@ import './Bookmark.css';
 import { useDispatch, useSelector } from 'react-redux';
 
 function BookmarkList() {
+
     const dispatch = useDispatch();
     const bookmarks = useSelector(state => state.bookmark.bookmarks);
-    const deleteBookmark = (bookMarkId) => {
+
+    /**
+     * Methos to remove bookmark
+     * @param {*} bookMarkId 
+     */
+    const deleteBookmark = (event, bookMarkId) => {
         console.log('deleteBookmark');
+        event.preventDefault();
         dispatch({
             type: 'DELETE_BOOKMARK',
             payload: {
                 id: bookMarkId
             }
 
-        })
+        });
 
     };
+
+    /**
+     * Method to display bookmarks
+     * @returns HTML ELEMENT for bookmark
+     */
 
     const bookmarkList = () => {
+
         return bookmarks.map(bookmark => (
             <div key={bookmark.id} className="bookmark-item">
-                <div>
-                    <i className="bi bi-x-lg float-end px-2" onClick={() => deleteBookmark(bookmark.id)}></i>
-                </div>
+                <a href={bookmark.url} target="blank">
+                    <div>
+                        <i className="bi bi-x-lg float-end px-2" onClick={(e) => deleteBookmark(e, bookmark.id)}></i>
+                    </div>
 
-                <div className="item-warpper">
-                    <div className="initial-letter">{bookmark.name.charAt(0)}</div>
-                    <div className="item-link"><a href={bookmark.url} target="blank">{bookmark.name}</a></div>
-                </div>
+                    <div className="item-warpper">
+                        <div className="initial-letter">{bookmark.name.charAt(0)}</div>
+                        <div className="item-link">{bookmark.name}</div>
+                    </div>
+                </a>
+
             </div>
-        ))
+        ));
     };
-
+    /**
+     * Method for empty bookmark list
+     * @returns HTML element for empty bookmark list
+     */
     const bookmarkEmptyList = () => {
         return (
             <div className="empty-list">
                 No bookmark found !!
             </div>
-        )
-
+        );
     };
 
-    console.log("Bookmark list", bookmarks);
     return (
         <div className="bookmark-list">
             {bookmarks.length > 0 ? bookmarkList() : bookmarkEmptyList()}
