e9305ed7290e41357b2d5c1e6e474d2c914c9945
GopeshMedayil <medayilgopesh@gmail.com>
2022-05-03 16:13:07 +0530

    added empty bookamrk functionality

--- src/components/Bookmark/BookmarkList.js
+++ src/components/Bookmark/BookmarkList.js
@@ -3,25 +3,39 @@ import { useSelector } from 'react-redux';
 
 function BookmarkList() {
 
+    const bookmarks = useSelector(state => state.bookmark.bookmarks);
     const deleteBookmark = () => {
         console.log('deleteBookmark');
-    }
-    const bookmarks = useSelector(state => state.bookmark.bookmarks);
+    };
+
+    const bookmarkList = () => {
+        return bookmarks.map(bookmark => (
+            <div key={bookmark.id} className="bookmark-item">
+                <div>
+                    <i className="bi bi-x-lg float-end px-2" onClick={deleteBookmark}></i>
+                </div>
+
+                <div className="item-warpper">
+                    <div className="initial-letter">{bookmark.name.charAt(0)}</div>
+                    <div className="item-link"><a href={bookmark.url} target="blank">{bookmark.name}</a></div>
+                </div>
+            </div>
+        ))
+    };
+
+    const bookmarkEmptyList = () => {
+        return (
+            <div className="empty-list">
+                No bookmark found !!
+            </div>
+        )
+
+    };
+
     console.log("Bookmark list", bookmarks);
     return (
         <div className="bookmark-list">
-            {bookmarks && bookmarks.map(bookmark => (
-                <div key={bookmark.id} className="bookmark-item">
-                    <div>
-                        <i className="bi bi-x-lg float-end px-2" onClick={deleteBookmark}></i>
-                    </div>
-
-                    <div className="item-warpper">
-                        <div className="initial-letter">{bookmark.name.charAt(0)}</div>
-                        <div className="item-link"><a href={bookmark.url} target="blank">{bookmark.name}</a></div>
-                    </div>
-                </div>
-            ))}
+            {bookmarks.length > 0 ? bookmarkList() : bookmarkEmptyList()}
 
         </div>
     )
